9800a1c38b32b7d7351eb8ed43492af5f4cafb03
Irene Wang <lohas1107@gmail.com>
2023-05-02 08:51:57 +0800

    test: user controller test

---
+++ spring/src/test/kotlin/tw/waterballsa/gaas/spring/controllers/UserControllerTest.kt
@@ -0,0 +1,32 @@
+package tw.waterballsa.gaas.spring.controllers
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles(profiles = ["dev"])
+class UserControllerTest {
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @Test
+    fun getUserGivenLogin() {
+        //todo: login first
+
+        mockMvc.perform(get("/users/1"))
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.id").value(1))
+            .andExpect(jsonPath("$.email").exists())
+    }
+
+}
