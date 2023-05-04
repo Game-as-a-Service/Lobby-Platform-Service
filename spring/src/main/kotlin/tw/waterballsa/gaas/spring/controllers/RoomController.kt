998e50eb9891923d4bf16239f18183736c52f697
akechetaki10736 <chi972121@gmail.com>
2023-05-04 20:51:47 +0800

    :sparkles: 建立 Controller 檔案

---
+++ spring/src/main/kotlin/tw/waterballsa/gaas/spring/controllers/RoomController.kt
@@ -0,0 +1,9 @@
+package tw.waterballsa.gaas.spring.controllers
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/rooms")
+class RoomController {
+}
