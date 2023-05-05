5b778268d651b1dfb512cf361376498a358665f5
Irene Wang <lohas1107@gmail.com>
2023-05-06 06:30:22 +0800

    chore: add return type

--- application/src/main/kotlin/tw/waterballsa/gaas/application/usecases/GetUserUseCase.kt
+++ application/src/main/kotlin/tw/waterballsa/gaas/application/usecases/GetUserUseCase.kt
@@ -1,12 +1,13 @@
 package tw.waterballsa.gaas.application.usecases
 
 import tw.waterballsa.gaas.application.repositories.UserRepository
+import tw.waterballsa.gaas.domain.User
 import javax.inject.Named
 
 @Named
 class GetUserUseCase(
     private val userRepository: UserRepository
 ) {
-    fun execute(id: String) = userRepository.findUserById(id)
+    fun execute(id: String): User? = userRepository.findUserById(id)
 }
 
--- spring/src/main/kotlin/tw/waterballsa/gaas/spring/controllers/UserController.kt
+++ spring/src/main/kotlin/tw/waterballsa/gaas/spring/controllers/UserController.kt
@@ -4,11 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 import tw.waterballsa.gaas.application.usecases.GetUserUseCase
+import tw.waterballsa.gaas.domain.User
 
 @RestController
 class UserController(
     private val getUserUseCase: GetUserUseCase
 ) {
     @GetMapping("/users/{id}")
-    fun getUser(@PathVariable id: String) = getUserUseCase.execute(id)
+    fun getUser(@PathVariable id: String): User? = getUserUseCase.execute(id)
 }
