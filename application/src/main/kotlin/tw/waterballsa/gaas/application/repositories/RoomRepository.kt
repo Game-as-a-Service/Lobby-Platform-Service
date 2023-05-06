65899fbd1f6750ae14d15b25f48909997bbb1138
施承毅 <harryshih@local>
2023-05-06 14:04:44 +0800

    fix: typo

--- application/src/main/kotlin/tw/waterballsa/gaas/application/repositories/RoomRepository.kt
+++ application/src/main/kotlin/tw/waterballsa/gaas/application/repositories/RoomRepository.kt
@@ -3,6 +3,6 @@ package tw.waterballsa.gaas.application.repositories
 import tw.waterballsa.gaas.domain.Room
 
 interface RoomRepository {
-:    fun findByRoomId(roomId: Room.RoomId): Room?
+    fun findByRoomId(roomId: Room.RoomId): Room?
     fun deleteAll()
 }
