3adcc83a075d47230b13403f85faac425b2a124f
Rick Su <35659961+ricksu978@users.noreply.github.com>
2023-05-10 21:04:19 +1000

    Resolve Reviews

--- spring/src/main/kotlin/tw/waterballsa/gaas/spring/eventbus/WebSocketEventBus.kt
+++ spring/src/main/kotlin/tw/waterballsa/gaas/spring/eventbus/WebSocketEventBus.kt
@@ -1,12 +1,13 @@
 package tw.waterballsa.gaas.spring.eventbus
 
+import org.springframework.stereotype.Component
 import tw.waterballsa.gaas.application.eventbus.EventBus
 import tw.waterballsa.gaas.events.DomainEvent
-import javax.inject.Named
 
-@Named
+@Component
 class WebSocketEventBus : EventBus {
+
+    // TODO broadcast the events!
     override fun broadcast(events: Collection<DomainEvent>) {
-        TODO("Not yet implemented")
     }
 }
--- spring/src/test/kotlin/tw/waterballsa/gaas/spring/controllers/WalkingSkeletonControllerTest.kt
+++ spring/src/test/kotlin/tw/waterballsa/gaas/spring/controllers/WalkingSkeletonControllerTest.kt
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
@@ -20,7 +19,7 @@ class WalkingSkeletonControllerTest {
     @Autowired
     lateinit var mockMvc: MockMvc
 
-    @MockBean
+    @Autowired
     lateinit var eventBus: EventBus
 
     @Test
