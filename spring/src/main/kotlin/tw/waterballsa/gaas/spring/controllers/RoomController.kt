f977a466a2e46948be1e30c5e18db2cc125ef367
Ted <azoocx791029@gmai.com>
2023-05-25 00:35:26 +0800

    1. 改用認證的outh0 token 拿取user id 2. 新增模擬outh0 token

--- spring/src/main/kotlin/tw/waterballsa/gaas/spring/controllers/RoomController.kt
+++ spring/src/main/kotlin/tw/waterballsa/gaas/spring/controllers/RoomController.kt
@@ -2,12 +2,15 @@ package tw.waterballsa.gaas.spring.controllers
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.core.oidc.user.OidcUser
 import org.springframework.web.bind.annotation.*
 import tw.waterballsa.gaas.application.usecases.JoinRoomUsecase
 import tw.waterballsa.gaas.domain.GameRegistration
 import tw.waterballsa.gaas.domain.Room
 import tw.waterballsa.gaas.events.DomainEvent
 import tw.waterballsa.gaas.events.JoinedRoomEvent
+import tw.waterballsa.gaas.exceptions.PlatformException
 import tw.waterballsa.gaas.spring.extensions.getEvent
 
 @RestController
@@ -16,10 +19,11 @@ class RoomController(
     private val joinRoomUsecase: JoinRoomUsecase
 ) {
     @PostMapping("/{roomId}/players")
-    fun joinRoom(@PathVariable roomId: String, @RequestBody request: JoinRoomRequest): ResponseEntity<Any> {
+    fun joinRoom(@PathVariable roomId: String, @RequestBody request: JoinRoomRequest, @AuthenticationPrincipal principal: OidcUser?): ResponseEntity<Any> {
         try {
             val presenter = JoinRoomPresenter()
-            joinRoomUsecase.execute(request.toRequest(roomId), presenter)
+            val joinerId = principal?.subject ?: throw PlatformException("User id is null")
+            joinRoomUsecase.execute(request.toRequest(roomId, joinerId), presenter)
             return presenter.viewModel
                 ?.let { ResponseEntity.ok(it) }
                 ?: ResponseEntity.noContent().build()
@@ -29,10 +33,9 @@ class RoomController(
     }
 
     class JoinRoomRequest(
-        private val userId: String,
-        private val password: String? = null,
+        val password: String? = null
     ) {
-        fun toRequest(roomId: String): JoinRoomUsecase.Request =
+        fun toRequest(roomId: String, userId: String): JoinRoomUsecase.Request =
             JoinRoomUsecase.Request(
                 roomId = roomId,
                 userId = userId,
