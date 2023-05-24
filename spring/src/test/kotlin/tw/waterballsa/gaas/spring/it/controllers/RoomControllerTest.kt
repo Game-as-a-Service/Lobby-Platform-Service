c237b3266e1e4bfdfa0e627b698997d15368a644
Ted <azoocx791029@gmai.com>
2023-05-25 00:35:26 +0800

    1. 改用認證的outh0 token 拿取user id 2. 新增模擬outh0 token

--- spring/src/test/kotlin/tw/waterballsa/gaas/spring/it/controllers/RoomControllerTest.kt
+++ spring/src/test/kotlin/tw/waterballsa/gaas/spring/it/controllers/RoomControllerTest.kt
@@ -8,10 +8,18 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
+import org.springframework.security.oauth2.core.oidc.OidcIdToken
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser
+import org.springframework.security.oauth2.core.oidc.user.OidcUser
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.ResultActions
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.RequestPostProcessor
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import tw.waterballsa.gaas.application.eventbus.EventBus
@@ -23,6 +31,7 @@ import tw.waterballsa.gaas.domain.Room
 import tw.waterballsa.gaas.domain.User
 import tw.waterballsa.gaas.spring.models.TestJoinRoomRequest
 import tw.waterballsa.gaas.spring.repositories.data.UserData
+import java.time.Instant
 
 @SpringBootTest
 @ActiveProfiles(profiles = ["dev"])
@@ -40,6 +49,7 @@ class RoomControllerTest(
     lateinit var testUser: User
     lateinit var testGame: GameRegistration
     lateinit var testRoom: Room
+    lateinit var requestPostProcessor: RequestPostProcessor
 
     @BeforeEach
     fun setUp() {
@@ -57,8 +67,9 @@ class RoomControllerTest(
         testRoom = createRoom()
         val userB = userRepository.createUser(User(User.Id("2"), "test2@mail.com", "winner1122"))
         val playerB = Room.Player(userB.id!!, userB.nickname)
-        val request = joinRoomRequest(userB.id!!.value);
-        joinRoom(request)
+        requestPostProcessor = oidcLogin(userB.id!!.value, userB.nickname, userB.email)
+        val request = joinRoomRequest();
+        joinRoom(request, requestPostProcessor)
             .thenJoinRoomSuccessfully()
     }
 
@@ -68,8 +79,9 @@ class RoomControllerTest(
         val errorPassword = "password"
         testRoom = createRoom(password)
         val userB = userRepository.createUser(User(User.Id("2"), "test2@mail.com", "winner1122"))
-        val request = joinRoomRequest(userB.id!!.value, errorPassword);
-        joinRoom(request)
+        requestPostProcessor = oidcLogin(userB.id!!.value, userB.nickname, userB.email)
+        val request = joinRoomRequest(errorPassword);
+        joinRoom(request, requestPostProcessor)
             .thenWrongPassword()
     }
     @Test
@@ -77,9 +89,9 @@ class RoomControllerTest(
         val password = "P@ssw0rd"
         testRoom = createRoom(password)
         val userB = userRepository.createUser(User(User.Id("2"), "test2@mail.com", "winner1122"))
-        val playerB = Room.Player(userB.id!!, userB.nickname)
-        val request = joinRoomRequest(userB.id!!.value, password);
-        joinRoom(request)
+        requestPostProcessor = oidcLogin(userB.id!!.value, userB.nickname, userB.email)
+        val request = joinRoomRequest(password);
+        joinRoom(request, requestPostProcessor)
             .thenJoinRoomSuccessfully()
     }
 
@@ -100,14 +112,17 @@ class RoomControllerTest(
         )
     )
 
-    private fun joinRoomRequest(userId: String, password: String? = null): TestJoinRoomRequest =
+    private fun joinRoomRequest(password: String? = null): TestJoinRoomRequest =
         TestJoinRoomRequest(
-            userId = userId,
             password = password
         )
 
-    private fun joinRoom(request: TestJoinRoomRequest): ResultActions =
-        mockMvc.perform(post("/rooms/${testRoom.id!!.value}/players").contentType(APPLICATION_JSON).content(request.toJson()))
+    private fun joinRoom(request: TestJoinRoomRequest, requestPostProcessor: RequestPostProcessor): ResultActions =
+        mockMvc.perform(post("/rooms/${testRoom.id!!.value}/players")
+            .with(requestPostProcessor)
+            .contentType(APPLICATION_JSON)
+            .content(request.toJson())
+        )
 
     private fun ResultActions.thenJoinRoomSuccessfully() {
         this.andExpect(status().isOk)
@@ -131,4 +146,15 @@ class RoomControllerTest(
             password = password
         )
     )
+
+    private fun oidcLogin(sub: String, name: String, email: String) = RequestPostProcessor { request ->
+        val claims: Map<String, Any> = mapOf("sub" to sub, "name" to name, "email" to email)
+        val idToken = OidcIdToken("token", Instant.now(), Instant.now().plusSeconds(60), claims)
+        val authorityAttributes: Map<String, Any> = claims
+        val authority = OAuth2UserAuthority(authorityAttributes)
+        val oidcUser: OidcUser = DefaultOidcUser(listOf(authority), idToken)
+        val authentication = OAuth2AuthenticationToken(oidcUser, listOf(SimpleGrantedAuthority("ROLE_USER")), "oidc")
+        SecurityContextHolder.getContext().authentication = authentication
+        request
+    }
 }
