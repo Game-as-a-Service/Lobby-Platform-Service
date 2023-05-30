b74bfb07be587ce2240ea21c7c01c8c4921edf35
Michael D <mikaeloduh@users.noreply.github.com>
2023-05-30 23:42:56 +0800

    refactor: use static import

--- spring/src/main/kotlin/tw/waterballsa/gaas/spring/controllers/HealthCheckController.kt
+++ spring/src/main/kotlin/tw/waterballsa/gaas/spring/controllers/HealthCheckController.kt
@@ -1,6 +1,6 @@
 package tw.waterballsa.gaas.spring.controllers
 
-import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.NO_CONTENT
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HealthCheckController {
     @GetMapping("/health")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseStatus(NO_CONTENT)
     fun healthCheck() {
     }
 }
