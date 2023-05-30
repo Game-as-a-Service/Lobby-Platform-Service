33638b32f80d19a18587f1e58d1b61ec03a9ddf8
Michael D <mikaeloduh@users.noreply.github.com>
2023-05-30 22:43:42 +0800

    refactor: simplify the code

--- spring/src/main/kotlin/tw/waterballsa/gaas/spring/controllers/HealthCheckController.kt
+++ spring/src/main/kotlin/tw/waterballsa/gaas/spring/controllers/HealthCheckController.kt
@@ -1,14 +1,14 @@
 package tw.waterballsa.gaas.spring.controllers
 
-import org.springframework.http.ResponseEntity
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HealthCheckController {
-
     @GetMapping("/health")
-    fun healthCheck(): ResponseEntity<Unit> {
-        return ResponseEntity.noContent().build()
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun healthCheck() {
     }
 }
