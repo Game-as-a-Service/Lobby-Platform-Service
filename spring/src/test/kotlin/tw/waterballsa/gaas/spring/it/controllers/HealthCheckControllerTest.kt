c8d77952522dcaa28c1de28cf48c0aa5a007e4e5
Michael D <mikaeloduh@users.noreply.github.com>
2023-05-30 22:48:49 +0800

    refactor: HealthCheckControllerTest to inherit from AbstractSpringBootTest

--- spring/src/test/kotlin/tw/waterballsa/gaas/spring/it/controllers/HealthCheckControllerTest.kt
+++ spring/src/test/kotlin/tw/waterballsa/gaas/spring/it/controllers/HealthCheckControllerTest.kt
@@ -1,19 +1,11 @@
 package tw.waterballsa.gaas.spring.it.controllers
 
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import tw.waterballsa.gaas.spring.it.AbstractSpringBootTest
 
-@SpringBootTest
-@AutoConfigureMockMvc
-class HealthCheckControllerTest {
-    @Autowired
-    lateinit var mockMvc: MockMvc
-
+class HealthCheckControllerTest : AbstractSpringBootTest() {
     @Test
     fun testHealthCheck() {
         mockMvc.perform(get("/health"))
