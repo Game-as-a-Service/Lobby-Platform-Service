a5427a5c3ebbf1ef434af5e99503a3dce2e7b008
Michael D <mikaeloduh@users.noreply.github.com>
2023-05-30 20:54:40 +0800

    refactor: clean up

--- spring/src/test/kotlin/tw/waterballsa/gaas/spring/it/controllers/HealthCheckControllerTest.kt
+++ spring/src/test/kotlin/tw/waterballsa/gaas/spring/it/controllers/HealthCheckControllerTest.kt
@@ -1,12 +1,9 @@
 package tw.waterballsa.gaas.spring.it.controllers
 
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
