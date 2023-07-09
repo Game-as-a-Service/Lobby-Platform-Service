d4580c3921a64ebafece7f3e72f6329921895d2f
KuoChe <kuoche1712003@gmail.com>
2023-07-10 00:19:15 +0800

    refactor: remove unused endpoint and login endpoint use response entity

--- spring/src/main/kotlin/tw/waterballsa/gaas/spring/controllers/OAuth2Controller.kt
+++ spring/src/main/kotlin/tw/waterballsa/gaas/spring/controllers/OAuth2Controller.kt
@@ -1,31 +1,20 @@
 package tw.waterballsa.gaas.spring.controllers
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpHeaders.*
 import org.springframework.http.HttpStatus.*
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.security.oauth2.core.oidc.OidcIdToken
-import org.springframework.security.oauth2.core.oidc.user.OidcUser
 import org.springframework.security.oauth2.jwt.Jwt
-import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.servlet.mvc.support.RedirectAttributes
-import org.springframework.web.servlet.view.RedirectView
-import org.springframework.web.util.UriComponentsBuilder
 import tw.waterballsa.gaas.application.usecases.CreateUserUseCase
 import tw.waterballsa.gaas.exceptions.PlatformException
-import tw.waterballsa.gaas.spring.utils.WebClientUtils
 
 @RestController
 class OAuth2Controller(
     private val createUserUseCase: CreateUserUseCase,
 ) {
-
-    @Value("\${frontend}")
-    private lateinit var frontendUrl: String
 
     @GetMapping
     fun home(@AuthenticationPrincipal principal: Jwt): String {
@@ -33,24 +22,11 @@ class OAuth2Controller(
         return principal.tokenValue ?: "index"
     }
 
-    @GetMapping("/login-successfully")
-    fun loginSuccessfully(@AuthenticationPrincipal principal: OidcUser): ResponseEntity<Any> {
-        sendTokenToFrontend(principal.idToken)
-        return ResponseEntity.status(FOUND).header(LOCATION, "/").build()
-    }
-
     @GetMapping("/login")
-    fun login(@RequestParam type: String, redirectAttributes: RedirectAttributes): RedirectView{
-        redirectAttributes["type"] = type
-        return RedirectView("oauth2/authorization/auth0")
-    }
-
-    private fun sendTokenToFrontend(oidcIdToken: OidcIdToken) {
-        val uriString = UriComponentsBuilder.fromUriString(frontendUrl)
-            .queryParam("token", oidcIdToken.tokenValue)
-            .toUriString()
-
-        WebClientUtils.get(uriString, String::class.java)
+    fun login(@RequestParam type: String): ResponseEntity<Unit>{
+        return ResponseEntity.status(FOUND)
+            .header(LOCATION, "/oauth2/authorization/auth0?type=$type")
+            .build()
     }
 }
 
