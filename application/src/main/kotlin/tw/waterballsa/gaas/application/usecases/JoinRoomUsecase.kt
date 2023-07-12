2fa2855c098d4a6b0be27645a1d04764a1b83618
TingIāu "Ting" Kì <51072200+frankvicky@users.noreply.github.com>
2023-07-13 01:01:24 +0800

    :sparkles: 實作一個人同時只能加入一個房間。 (#123)
    * :sparkles: 實作一個人同時只能加入一個房間。
    
    * :recycle: 依照 PR Comment 修改

--- application/src/main/kotlin/tw/waterballsa/gaas/application/usecases/JoinRoomUsecase.kt
+++ application/src/main/kotlin/tw/waterballsa/gaas/application/usecases/JoinRoomUsecase.kt
@@ -18,11 +18,20 @@ class JoinRoomUsecase(
 ) {
 
     fun execute(request: Request) {
-        with(request) {
-            val room = findRoomById(Room.Id(roomId))
-            room.validateRoomPassword(password)
-            room.validateFullRoom()
-            room.joinPlayer(userId)
+        val (roomId, userId, password) = request
+        val room = findRoomById(Room.Id(roomId))
+        validateUserJoinedRoom(userId)
+        room.run {
+            validateRoomPassword(password)
+            validateFullRoom()
+            joinPlayer(userId)
+        }
+    }
+
+    private fun validateUserJoinedRoom(userId: String) {
+        val hasJoined = roomRepository.hasPlayerJoinedRoom(User.Id(userId))
+        if (hasJoined) {
+            throw PlatformException("Player($userId) has joined another room.")
         }
     }
 
