59f6d42abb1db196a6188f6ab08d043ba6a30c71
wiromi <>
2023-07-20 00:24:14 +0800

    初始 socket io

--- spring/src/main/kotlin/tw/waterballsa/gaas/spring/LobbyPlatformApplication.kt
+++ spring/src/main/kotlin/tw/waterballsa/gaas/spring/LobbyPlatformApplication.kt
@@ -1,11 +1,20 @@
 package tw.waterballsa.gaas.spring
 
+import com.corundumstudio.socketio.Configuration
+import com.corundumstudio.socketio.SocketIOServer
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Import
+import tw.waterballsa.gaas.spring.configs.socketio.SocketIOConfig
 
 
 @SpringBootApplication(scanBasePackages = ["tw.waterballsa.gaas"])
+@Import(SocketIOConfig::class)
 class LobbyPlatformApplication
+
+
 
 fun main(args: Array<String>) {
     runApplication<LobbyPlatformApplication>(*args)
