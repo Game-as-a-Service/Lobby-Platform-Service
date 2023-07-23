7a6cc73aaefa6665831d82e5604045eb76c5bf15
wiromi <>
2023-07-23 21:42:36 +0800

    socket io update

--- spring/src/main/kotlin/tw/waterballsa/gaas/spring/configs/socketio/SocketIOEventHandler.kt
+++ spring/src/main/kotlin/tw/waterballsa/gaas/spring/configs/socketio/SocketIOEventHandler.kt
@@ -2,6 +2,9 @@ package tw.waterballsa.gaas.spring.configs.socketio
 
 import com.corundumstudio.socketio.SocketIOClient
 import com.corundumstudio.socketio.SocketIOServer
+import com.nimbusds.jose.shaded.json.JSONObject
+import io.swagger.v3.core.util.Json
+import org.bson.json.JsonObject
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
@@ -25,21 +28,28 @@ class SocketIOEventHandler(
         }
 
 
-        socketIOServer.addEventListener("CHAT_MESSAGE", String::class.java) { client: SocketIOClient, data: String, _ ->
+        socketIOServer.addEventListener("CHAT_MESSAGE", JSONObject::class.java) { client: SocketIOClient, data: JSONObject, _ ->
             // Handle the "chatMessage" event
             println("Received message: $data from client: ${client.sessionId}")
+            println(" CHAT_MESSAGE message is " + data)
 
             // ECHO
             client.sendEvent("CHAT_MESSAGE", data)
         }
 
-        socketIOServer.addEventListener("CHATROOM_JOIN", UserMessage::class.java) {
-            client: SocketIOClient, data: UserMessage, _ ->
+        socketIOServer.addEventListener("CHATROOM_JOIN", JSONObject::class.java) {
+                client: SocketIOClient, data: JSONObject, _ ->
             // ECHO
+            println(" CHATROOM_JOIN message is  " + data)
             client.sendEvent("CHAT_MESSAGE", data)
+
+
         }
 
 
         // Add other event handlers here
+
+
+
     }
 }
