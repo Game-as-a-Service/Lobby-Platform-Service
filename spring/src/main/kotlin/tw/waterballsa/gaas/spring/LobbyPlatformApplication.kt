beec0da3c9185dad09ee1fb22364b44ce6d0a2f1
wiromi <>
2023-07-31 22:52:54 +0800

    設定socket io 啟動 server 就會啟動

--- spring/src/main/kotlin/tw/waterballsa/gaas/spring/LobbyPlatformApplication.kt
+++ spring/src/main/kotlin/tw/waterballsa/gaas/spring/LobbyPlatformApplication.kt
@@ -2,9 +2,6 @@ package tw.waterballsa.gaas.spring
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Import
-import tw.waterballsa.gaas.spring.configs.socketio.SocketIOConfig
-
 
 @SpringBootApplication(scanBasePackages = ["tw.waterballsa.gaas"])
 class LobbyPlatformApplication
