7e2ac926cba9fb0df24532258821164a0b21f06c
wiromi <>
2023-08-07 09:07:07 +0800

    優化語法

--- spring/src/main/kotlin/tw/waterballsa/gaas/spring/configs/socketio/SocketIOConfig.kt
+++ spring/src/main/kotlin/tw/waterballsa/gaas/spring/configs/socketio/SocketIOConfig.kt
@@ -8,15 +8,17 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 @EnableConfigurationProperties(SocketIOProperties::class)
-class SocketIOConfig (private val socketIOProperties: SocketIOProperties){
+class SocketIOConfig (){
 
     @Bean
-    fun SocketIOConfig(): SocketIOServer {
+    fun socketIOServer(socketIOProperties: SocketIOProperties): SocketIOServer {
         val configuration = com.corundumstudio.socketio.Configuration()
             .apply {
-        hostname = socketIOProperties.host
-        port = socketIOProperties.port
-    }
+                with(socketIOProperties){
+                    hostname = socketHost
+                    port = socketPort
+                }
+        }
         return SocketIOServer(configuration)
     }
 }
