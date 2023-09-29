1b4c7aeccf7fdd1cc41e34c63ec093bef03f960a
wiroger9595@gmail.com <under987..A>
2023-09-29 13:07:31 +0800

    update join room, create room and leave room

--- spring/src/main/kotlin/tw/waterballsa/gaas/spring/configs/socketio/SocketIOEventHandler.kt
+++ spring/src/main/kotlin/tw/waterballsa/gaas/spring/configs/socketio/SocketIOEventHandler.kt
@@ -10,20 +10,18 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.stereotype.Component
-import org.springframework.web.context.request.RequestContextHolder
 import tw.waterballsa.gaas.application.repositories.RoomRepository
 import tw.waterballsa.gaas.application.repositories.UserRepository
 import tw.waterballsa.gaas.application.usecases.GetRoomUsecase
 import tw.waterballsa.gaas.domain.Room
-import tw.waterballsa.gaas.events.SocketioEvent
-import javax.servlet.http.HttpServletRequest
+import tw.waterballsa.gaas.spring.controllers.identityProviderId
+import tw.waterballsa.gaas.spring.controllers.presenter.GetRoomPresenter
 
 
 @Component
-class SocketIOEventHandler(
-    private val socketIOServer: SocketIOServer,
-    protected val roomRepository: RoomRepository,
-    protected val userRepository: UserRepository,
+class SocketIOEventHandler(private val socketIOServer: SocketIOServer,
+                           protected val roomRepository: RoomRepository,
+                           protected val userRepository: UserRepository,
 ) {
 
     private val logger: Logger = LoggerFactory.getLogger(SocketIOEventHandler::class.java)
@@ -36,66 +34,118 @@ class SocketIOEventHandler(
 
     fun configureEventHandlers() {
 
-        socketIOServer.addConnectListener { client ->
-            val token =  client.handshakeData.httpHeaders.get(HttpHeaderNames.COOKIE)
-            val customHeader = client.handshakeData.getSingleUrlParam("Authorization")
-
-            if (client != null ) {
-                logger.info("有新用戶連結  , SessionId: {}", client.getSessionId())
+        socketIOServer.addConnectListener { client -> // 判断是否有客户端连接
+            if (client != null) {
+                logger.info("有新用户连接, SessionId: {}", client.getSessionId())
                 val board = socketIOServer.broadcastOperations
                 logger.info("board clientId {}", board.clients)
+
+            } else {
+                logger.error("并没有人连接上。。。")
             }
 
         }
 
-        socketIOServer.addEventListener(SocketIOEventName.CHAT_MESSAGE.eventName, SocketioEvent::class.java)
-            { client: SocketIOClient, socketioEvent: SocketioEvent, _ ->
+        socketIOServer.addEventListener(SocketIOEventName.CHAT_MESSAGE.eventName, JSONObject::class.java)
+            { client: SocketIOClient, chatMessage: JSONObject, _ ->
             // Handle the "chatMessage" event
-            logger.info(" CHAT_MESSAGE Received message: $socketioEvent from client: ${client.sessionId}")
+            logger.info(" CHAT_MESSAGE Received message: $chatMessage from client: ${client.sessionId}")
+
             client.handshakeData.getSingleUrlParam("")
+
+
+            val jsonObject = JSONObject(chatMessage)
+
+            val map: Map<String, Any> = jsonObject.toMap()
+            val type = map["type"]
+            val data = map["data"] as? Map<String, Any>
+
+            val target = data?.get("target")
+
+
             // ECHO
-            client.sendEvent(SocketIOEventName.CHAT_MESSAGE.eventName, socketioEvent.data)
+            client.sendEvent(SocketIOEventName.CHAT_MESSAGE.eventName, data)
         }
 
-        socketIOServer.addEventListener(SocketIOEventName.JOIN_ROOM.eventName, SocketioEvent::class.java) {
-                client: SocketIOClient, socketioEvent: SocketioEvent, _ ->
+        socketIOServer.addEventListener(SocketIOEventName.JOIN_ROOM.eventName, JSONObject::class.java) {
+                client: SocketIOClient, getJoinRoom: JSONObject, _ ->
 
             // ECHO
-            logger.info(" JOIN_ROOM Received message: $socketioEvent from client: ${client.sessionId}")
-            val roomSize = client.getCurrentRoomSize(socketioEvent.data.target)
+            logger.info(" JOIN_ROOM Received message: $getJoinRoom from client: ${client.sessionId}")
 
+            val jsonObject = JSONObject(getJoinRoom)
+
+            val map: Map<String, Any> = jsonObject.toMap()
+            val user = map["user"]
+            val targetRoom = map["target"]
+
+            val userInfo = JSONObject(user as MutableMap<String, *>?)
+            val userInfoMap: Map<String, Any> = userInfo.toMap()
+
+            val id = userInfoMap["id"]
+            val nickname = userInfoMap["nickname"]
+
+            val target = map?.get("target")
+
+            // need mongo
+//            val roomId = Room.Id(target.toString())
+//            val room = roomRepository.findById(roomId)
+//
+            val roomSize = client.getCurrentRoomSize(target.toString())
+
+            // roomSize == 0, create room  else join room
             if(roomSize == 0){
-                logger.info("user： " +  client.sessionId + "you are the host ")
+                logger.info("用户：{}", client.sessionId, "you are the host ")
+                //client.send()
             } else{
-                client.joinRoom(socketioEvent.data.target)
-                logger.info("Client joined room: ${socketioEvent.data.target}")
-                logger.info("id = " + socketioEvent.data.user.id + " nickname " + socketioEvent.data.user.nickname +  " targetRoom  " + socketioEvent.data.target)
-                logger.info(" room size is : ${client.getCurrentRoomSize(socketioEvent.data.target)}")
-                socketIOServer.getRoomOperations(socketioEvent.data.target).sendEvent(SocketIOEventName.JOIN_ROOM.eventName, socketioEvent.data.user.id)
+                client.joinRoom(target.toString())
+                logger.info("Client joined room: ${target.toString()}")
+                logger.info("id = " + id + " nickname " + nickname +  " targetRoom  " + targetRoom)
+                logger.info(" room size is : ${client.getCurrentRoomSize(target.toString())}")
+                socketIOServer.getRoomOperations(target.toString()).sendEvent(SocketIOEventName.JOIN_ROOM.eventName, id)
             }
+
         }
 
 
-        socketIOServer.addEventListener(SocketIOEventName.LEAVE_ROOM.eventName, SocketioEvent::class.java) {
-                client: SocketIOClient, socketioEvent: SocketioEvent, _ ->
+        socketIOServer.addEventListener(SocketIOEventName.LEAVE_ROOM.eventName, JSONObject::class.java) {
+                client: SocketIOClient, leaveRoom: JSONObject, _ ->
             // ECHO
-            logger.info(" LEAVE_ROOM Received message: ${socketioEvent.data.target} from client: ${client.sessionId}")
+            logger.info(" LEAVE_ROOM Received message: $leaveRoom from client: ${client.sessionId}")
 
-            client.leaveRoom(socketioEvent.data.target)
-            socketIOServer.removeNamespace(socketioEvent.data.target)
+            val jsonObject = JSONObject(leaveRoom)
+
+            val map: Map<String, Any> = jsonObject.toMap()
+            val type = map["type"]
+            val data = map["data"] as? Map<String, Any>
+
+            val target = data?.get("target")
+
+            client.leaveRoom(target.toString())
+
+            socketIOServer.removeNamespace(target.toString())
         }
 
 
-        socketIOServer.addEventListener(SocketIOEventName.DISCONNECT.eventName, SocketioEvent::class.java) {
-                client: SocketIOClient, socketioEvent: SocketioEvent, _ ->
+        socketIOServer.addEventListener("DISCONNECT", JSONObject::class.java) {
+                client: SocketIOClient, leaveRoom: JSONObject, _ ->
 
             client.disconnect()
-            logger.info(" client is leaven room with key disconnect")
+            println(" client is leaven room with key disconnect")
         }
+
+
+
+
 
         socketIOServer.addDisconnectListener {
                 client: SocketIOClient ->
-            logger.info("Server disconnected on the server side")
+            println("Server disconnected on the server side")
         }
+
+
+
+
+
     }
 }
