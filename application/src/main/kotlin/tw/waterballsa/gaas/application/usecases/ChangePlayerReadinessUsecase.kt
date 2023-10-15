4086edca387acf34574c209ae23a70e9e4bce0b7
Ted <82504888+ted791029@users.noreply.github.com>
2023-10-15 21:40:44 +0800

    實作[WebSocket] 看見其他玩家準備/取消準備 (#182)
    * 實作[WebSocket] 看見其他玩家準備/取消準備

--- application/src/main/kotlin/tw/waterballsa/gaas/application/usecases/ChangePlayerReadinessUsecase.kt
+++ application/src/main/kotlin/tw/waterballsa/gaas/application/usecases/ChangePlayerReadinessUsecase.kt
@@ -1,13 +1,21 @@
 package tw.waterballsa.gaas.application.usecases
 
+import tw.waterballsa.gaas.application.eventbus.EventBus
 import tw.waterballsa.gaas.application.repositories.RoomRepository
 import tw.waterballsa.gaas.application.repositories.UserRepository
+import tw.waterballsa.gaas.domain.Room
+import tw.waterballsa.gaas.events.PlayerReadinessChangedEvent
+import tw.waterballsa.gaas.events.PlayerReadinessChangedEvent.Data
+import tw.waterballsa.gaas.events.PlayerReadinessChangedEvent.Data.User
+import tw.waterballsa.gaas.events.enums.EventMessageType.USER_NOT_READY
+import tw.waterballsa.gaas.events.enums.EventMessageType.USER_READY
 import javax.inject.Named
 
 @Named
 class ChangePlayerReadinessUsecase(
     roomRepository: RoomRepository,
     userRepository: UserRepository,
+    private val eventBus: EventBus,
 ) : AbstractRoomUseCase(roomRepository, userRepository) {
     fun execute(request: Request) {
         with(request) {
@@ -15,6 +23,9 @@ class ChangePlayerReadinessUsecase(
             val player = findPlayerByIdentity(userIdentity)
             room.changePlayerReadiness(player.id, readiness)
             roomRepository.update(room)
+
+            val playerReadinessChangedEvent = room.changePlayerReadiness(readiness, player.id!!.value, player.nickname)
+            eventBus.broadcast(playerReadinessChangedEvent)
         }
     }
 
@@ -29,4 +40,15 @@ class ChangePlayerReadinessUsecase(
             fun cancelReady(roomId: String, userIdentity: String): Request = Request(roomId, userIdentity, false)
         }
     }
+}
+
+fun Room.changePlayerReadiness(
+    readiness: Boolean,
+    playerId: String,
+    nickname: String
+): PlayerReadinessChangedEvent {
+    val type = if (readiness) USER_READY else USER_NOT_READY
+    val user = User(playerId, nickname)
+    val data = Data(user, roomId!!.value)
+    return PlayerReadinessChangedEvent(type, data)
 }
